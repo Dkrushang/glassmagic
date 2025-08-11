0e49708d3cd6ad88e9750697b4fc7fbcd9ec51ab
Dkrushang <kru.desai1@gmail.com>
2025-08-11 13:42:55 -0400

    Working coy
    blueprint copy

---
+++ buildSrc/src/main/java/MyApp.kt
@@ -0,0 +1,7 @@
+@file:Suppress("unused")
+
+object MyApp {
+    const val appId = "dev.jahir.blueprint.app"
+    const val version = 245
+    const val versionName = "2.4.5"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,19 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.10.0"
+    const val kotlin = "2.1.21"
+    const val ksp = "$kotlin-2.0.1"
+
+    // OneSignal
+    const val oneSignal = "4.8.12"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 36
+    const val buildTools = "36.0.0"
+
+    // Blueprint
+    const val blueprint = "2.4.5"
+}
